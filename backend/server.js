7ad11ed194fefab9aede3490f24bc752b5d7c177
anikett35 <aniketbedwal18@gmail.com>
2025-12-05 18:53:21 +0530

    Add CORS for Vercel

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://pet-care-roan-three.vercel.app'
+    'https://pet-care-new.vercel.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
